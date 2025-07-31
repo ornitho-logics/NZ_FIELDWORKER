577d0a50c35f07a5220b0e227fcd0a1e74bab361
Mihai Valcu <valcu@orn.mpg.de>
2025-07-31 10:19:12 +0200

    -

--- main/R/MAP_leaflet.R
+++ main/R/MAP_leaflet.R
@@ -2,5 +2,19 @@
 leaflet_map <- function( ) {
   leaflet(options = leafletOptions(zoomControl = TRUE)) |>
     setView(lng = 172, lat = -44, zoom = 6) |>
-    addTiles(group = "OpenStreetMap")
+    addTiles(group = "OpenStreetMap") |>
+      addControlGPS(
+        options = gpsOptions(
+          position   = "topleft",
+          activate   = TRUE,
+          autoCenter = TRUE,
+          maxZoom    = 16,
+          setView    = TRUE
+        )
+      ) |>
+      addMiniMap(toggleDisplay = TRUE) |>
+      
+      addFullscreenControl()
+
+
 }
