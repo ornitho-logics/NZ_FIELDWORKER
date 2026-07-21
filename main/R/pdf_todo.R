65757debb3d401ed8cf345822848d17dd71c45e5
leberhartphillips <luke.eberhart@gmail.com>
2026-07-21 17:02:06 +0200

    WIP preserve todo SQL and PDF changes after main sync

--- main/R/pdf_todo.R
+++ main/R/pdf_todo.R
@@ -28,16 +28,15 @@ todo_pdf_prepare <- function(todo = DBq("SELECT * FROM TODO_LIST")) {
   rows <- todo_dt[,
                   .(
                     Todo = todo,
-                    Priority = priority,
                     Overdue = overdue_label,
                     Nest = nest_id,
                     State = nest_state,
                     Clutch = clutch_size,
                     Brood = brood_size,
                     Hatch = min_days_to_hatch,
-                    Visit = last_visit_days_ago,
-                    M = M_mark,
-                    F = F_mark,
+                    `Last Visit` = last_visit_days_ago,
+                    Male = M_mark,
+                    Female = F_mark,
                     Notes = notes
                   )
   ]
@@ -56,11 +55,69 @@ todo_pdf_prepare <- function(todo = DBq("SELECT * FROM TODO_LIST")) {
   )
 }
 
-
-todo_pdf_r <- function(x) {
-  paste(deparse(x), collapse = "\n")
+todo_pdf_escape_latex <- function(x) {
+  x <- as.character(x)
+  x[is.na(x)] <- ""
+  x <- gsub("[\r\n]+", " ", x)
+  x <- gsub("\\\\", "<<BACKSLASH>>", x)
+  x <- gsub("~", "<<TILDE>>", x, fixed = TRUE)
+  x <- gsub("\\^", "<<CARET>>", x, perl = TRUE)
+  x <- gsub("([#$%&_{}])", "\\\\\\1", x, perl = TRUE)
+  x <- gsub("<<BACKSLASH>>", "\\\\textbackslash{}", x, fixed = TRUE)
+  x <- gsub("<<TILDE>>", "\\\\textasciitilde{}", x, fixed = TRUE)
+  x <- gsub("<<CARET>>", "\\\\textasciicircum{}", x, fixed = TRUE)
+  x
 }
 
+todo_pdf_table_latex <- function(todo_name, x) {
+  header_labels <- names(x)
+  header_labels[header_labels == "Clutch"] <- "\\rotatebox[origin=c]{90}{Clutch}"
+  header_labels[header_labels == "Brood"] <- "\\rotatebox[origin=c]{90}{Brood}"
+  
+  if (!(todo_name %in% c("notA nest-check", "Hiding spot photos needed"))) {
+    header_labels[header_labels == "Hatch"] <- "Est. Hatch"
+  }
+  
+  header <- paste(header_labels, collapse = " & ")
+  body <- apply(as.matrix(x), 1, function(row) {
+    paste0(paste(todo_pdf_escape_latex(row), collapse = " & "), " \\\\")
+  })
+  
+  c(
+    "\\begingroup",
+    "\\setlength{\\tabcolsep}{3pt}",
+    "\\renewcommand{\\arraystretch}{1.08}",
+    "\\begin{longtable}{>{\\centering\\arraybackslash}p{2.0cm}>{\\centering\\arraybackslash}p{1.1cm}>{\\centering\\arraybackslash}p{0.75cm}>{\\centering\\arraybackslash}p{0.45cm}>{\\centering\\arraybackslash}p{0.45cm}>{\\centering\\arraybackslash}p{1.45cm}>{\\centering\\arraybackslash}p{1.45cm}>{\\centering\\arraybackslash}p{2.45cm}>{\\centering\\arraybackslash}p{2.45cm}>{\\RaggedRight\\arraybackslash}p{4.45cm}}",
+    "\\toprule",
+    paste0(header, " \\\\"),
+    "\\midrule",
+    "\\endfirsthead",
+    "\\toprule",
+    paste0(header, " \\\\"),
+    "\\midrule",
+    "\\endhead",
+    "\\midrule",
+    "\\multicolumn{10}{r}{\\footnotesize\\emph{Continued on next page}} \\\\",
+    "\\endfoot",
+    "\\bottomrule",
+    "\\endlastfoot",
+    body,
+    "\\end{longtable}",
+    "\\endgroup"
+  )
+}
+
+todo_pdf_heading <- function(todo_name) {
+  switch(
+    todo_name,
+    "Hiding spot photos needed" = "Broods to photograph: find these broods and take in-situ and tent photos",
+    "Unprocessed nest" = "Nests to process: egg photos and floatation needed",
+    "Untrapped parent" = "Nests with parents to capture or resight: band unmarked parents or determine identity with resighting",
+    "nest check" = "Nests to check for potential hatch",
+    "notA nest-check" = "Nests requiring a 'notA' closure visit",
+    todo_name
+  )
+}
 
 todo_pdf_body <- function(rows) {
   if (!nrow(rows)) {
@@ -71,16 +128,12 @@ todo_pdf_body <- function(rows) {
   table_cols <- setdiff(names(rows), "Todo")
   
   for (todo in unique(rows$Todo)) {
+    todo_rows <- as.data.frame(rows[Todo == todo, ..table_cols])
     out <- c(
       out,
-      glue("## {todo}"),
+      glue("## {todo_pdf_heading(todo)}"),
       "",
-      "```{r}",
-      glue(
-        "todo_rows[todo_rows$Todo == {todo_pdf_r(todo)}, ",
-        "{todo_pdf_r(table_cols)}, drop = FALSE]"
-      ),
-      "```",
+      todo_pdf_table_latex(todo, todo_rows),
       ""
     )
   }
@@ -124,7 +177,6 @@ todo_pdf_save <- function(
   qmd <- file.path(workdir, "todo.qmd")
   output <- file.path(workdir, "todo.pdf")
   
-  saveRDS(as.data.frame(pdf$rows), file.path(workdir, "todo_rows.rds"))
   writeLines(todo_pdf_qmd(pdf), qmd)
   quarto_render(
     input = qmd,
