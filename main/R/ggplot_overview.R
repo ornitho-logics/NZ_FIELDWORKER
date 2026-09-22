d72e1f95baf666fe299f7d4fcc09fb06f8ef471d
leberhartphillips <luke.eberhart@gmail.com>
2026-09-22 15:25:55 +0200

    overview test

--- main/R/ggplot_overview.R
+++ main/R/ggplot_overview.R
@@ -298,11 +298,11 @@ overview_cumulative_plot <- function(
     ) +
     overview_summary_annotation(summary_label, annotation_date) +
     scale_color_manual(
-      name = "Sex",
+      name = "Sexy",
       values = c(Female = "#c43c39", Male = "#2878b5")
     ) +
     scale_fill_manual(
-      name = "Sex",
+      name = "Sexy",
       values = c(Female = "#c43c39", Male = "#2878b5")
     ) +
     overview_date_scale() +
